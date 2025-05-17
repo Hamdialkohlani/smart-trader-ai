0b8f2c7d72e2ee28c61781bb4e2e53ac70e079a7
https://github.com/Handialkohlani/Wi-Fi-wasal/compare/main...update-readme <hamdialkohlany7769@gmail.com>
2025-05-17 20:33:09 +0300

    Create firebase_config.py

---
+++ firebase_config.py
@@ -0,0 +1,9 @@
+firebase_config = {
+    "apiKey": "AIzaSyA_NZvWdFUzvsP4M8-P5ddMzlQWBI7hCcI",
+    "authDomain": "smart-trader-ai-26fc4.firebaseapp.com",
+    "projectId": "smart-trader-ai-26fc4",
+    "storageBucket": "smart-trader-ai-26fc4.appspot.com",
+    "messagingSenderId": "445020353183",
+    "appId": "1:445020353183:web:8e36fc7727e320c8dce8aa",
+    "databaseURL": ""  # يمكن تركه فارغًا إذا لم تستخدم قاعدة Realtime Database
+}
